6dd2b40c9736102061a6bf8c5dd024649190e784
Chae Su Min <oxforo@naver.com>
2022-03-12 23:17:02 +0900

    스프링 빈과 스프링 컨테이너 사용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,21 +8,31 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 
+@Configuration
 public class AppConfig {
 
+	@Bean
 	public MemberService memberService() {
+		System.out.println("call AppConfig.memberService");
 		return new MemberServiceImpl(memberRepository());
 	}
 
+	@Bean
 	public MemberRepository memberRepository() {
+		System.out.println("call AppConfig.memberRepository");
 		return new MemoryMemberRepository();
 	}
 
+	@Bean
 	public OrderService orderService() {
+		System.out.println("call AppConfig.orderService");
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
 
+	@Bean
 	public DiscountPolicy discountPolicy() {
 		return new FixDiscountPolicy();
 	}
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
@@ -7,9 +10,12 @@ import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
 	public static void main(String[] args) {
-		AppConfig appConfig = new AppConfig();
+		// AppConfig appConfig = new AppConfig();
+		// MemberService memberService = appConfig.memberService();
 
-		MemberService memberService = appConfig.memberService();
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+		MemberService memberService = applicationContext.getBean("memberService", MemberService.class);// 이름 , 타입
+
 		Member member = new Member(1L, "memberA", Grade.VIP);
 		memberService.join(member);
 
